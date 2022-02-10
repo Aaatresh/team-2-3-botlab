577eb3797f5114b09ee950dc20212e815606f6c4
devansh <devansh@umich.edu>
2022-02-10 23:32:30 +0000

    Update src/slam/sensor_model.cpp

--- src/slam/sensor_model.cpp
+++ src/slam/sensor_model.cpp
@@ -14,6 +14,42 @@ SensorModel::SensorModel(void)
 double SensorModel::likelihood(const particle_t& sample, const lidar_t& scan, const OccupancyGrid& map)
 {
     ///////////// TODO: Implement your sensor model for calculating the likelihood of a particle given a laser scan //////////
-    
-    return 1.0;
+
+    double scanScore = 0.0;
+    int rayStride = 10;
+    MovingLaserScan movingScan(scan, sample.parent_pose, same.pose, rayStride);
+
+    for (const auto&ray : movingScan){
+        // compute ray score
+
+        scanScore += scoreRay(ray, map);
+
+    }    
+    return scanScore;
 }
+
+double scoreRay(adjusted_ray& ray, OccupancyGrid& map){
+ 
+    float s_too_near = -8;
+    float s_correct = -4;
+    float s_too_far = -12;
+
+    double d = get_dist_from_map(ray, map);
+    float thresh = map.metersPerCell_; # meters
+    if (ray.range <= d - thresh){
+        return s_too_near;
+    }
+    if (ray.range >= d + thresh){
+        return s_too_far;
+    }
+    return s_correct;
+}
+
+
+double get_dist_from_map(adjusted_ray_t ray, OccupancyGrid& map){
+
+    
+
+}
+
+
