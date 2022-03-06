a87c674c0bb9c200f67a6e2c0bfeb5e45e770a32
anirudh <aaatresh@umich.edu>
2022-03-06 17:52:56 -0500

    ..

--- launch_botlab.sh
+++ launch_botlab.sh
@@ -4,6 +4,6 @@ source setenv.sh
 wait
 ./bin/timesync &> /dev/null &
 ./bin/rplidar_driver &> /dev/null &
-./bin/slam --num-particles 50  --hit-odds 1 &> /dev/null &
-./bin/motion_controller &
+#./bin/slam --mapping-only &> /dev/null &
+#./bin/motion_controller &
 ./bin/botgui
